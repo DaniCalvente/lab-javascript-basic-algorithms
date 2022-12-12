06a1e60e90ea08bf1d5095421aa755241e52db92
Dani Calvente <danicalvente@hotmail.com>
2022-12-12 12:28:20 +0100

    Bonus 1

--- index.js
+++ index.js
@@ -72,3 +72,15 @@ for (let i = 0; i < longestName.length; i++) {
 }
 
 // Bonus 1. lorem ipsum generator
+
+let longText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet est at justo tempor mollis. Aliquam consequat lacinia interdum. Fusce sodales dui a elit pulvinar ornare. Integer ullamcorper nunc vel finibus venenatis. Sed lectus velit, lacinia non posuere a, porta fringilla turpis. Cras sagittis erat malesuada risus posuere fringilla. Etiam vulputate dolor metus, in pharetra velit dictum non. Vivamus eu augue nec est tempus viverra. Phasellus eu augue volutpat, fermentum risus et, dapibus purus. Quisque vel augue mauris. Maecenas mattis ipsum sit amet metus dictum, quis pharetra enim cursus. Sed ac ullamcorper turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. In consectetur diam ac turpis malesuada rutrum. Sed porta nulla id enim vulputate, eu tristique tortor cursus.";
+
+let wordsCounter = 1;
+
+for (let i = 0; i < longText.length; i++) {
+  if (longText[i] === " ") {
+    wordsCounter++;
+  }
+}
+console.log("Total Words: " + wordsCounter);
